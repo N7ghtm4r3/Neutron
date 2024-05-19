6cb9aca108b8be58cf0385831ec4bd1e31c624ad
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2024-05-19 23:37:20 +0200

    Working on TicketRevenue

--- Neutron-core/src/main/java/com/tecknobit/neutroncore/records/revenues/TicketRevenue.java
+++ Neutron-core/src/main/java/com/tecknobit/neutroncore/records/revenues/TicketRevenue.java
@@ -4,19 +4,36 @@ import java.util.List;
 
 public class TicketRevenue extends GeneralRevenue {
 
-    public static final String TICKET_LABEL_COLOR = "#B5A422";
+    public static final String PENDING_TICKET_LABEL_COLOR = "#B5A422";
 
-    private static final RevenueLabel label = new RevenueLabel(
-            "label_ticket_id",
+    public static final String CLOSED_TICKET_LABEL_COLOR = "#12b543";
+
+    private static final RevenueLabel PENDING_TICKET_LABEL = new RevenueLabel(
+            "pending_ticket_id",
             "Ticket",
-            TICKET_LABEL_COLOR
+            PENDING_TICKET_LABEL_COLOR
+    );
+
+    private static final RevenueLabel CLOSED_TICKET_LABEL = new RevenueLabel(
+            "closed_ticket_id",
+            "Ticket",
+            CLOSED_TICKET_LABEL_COLOR
     );
 
     private final long closingDate;
 
+    private boolean isClosed;
+
+    public TicketRevenue(String id, String title, double value, long openingDate, String description) {
+        super(id, title, value, openingDate, List.of(PENDING_TICKET_LABEL), description);
+        this.closingDate = -1;
+        this.isClosed = false;
+    }
+
     public TicketRevenue(String id, String title, double value, long openingDate, String description, long closingDate) {
-        super(id, title, value, openingDate, List.of(label), description);
+        super(id, title, value, openingDate, List.of(CLOSED_TICKET_LABEL), description);
         this.closingDate = closingDate;
+        this.isClosed = true;
     }
 
     public long getDuration() {
@@ -29,6 +46,14 @@ public class TicketRevenue extends GeneralRevenue {
 
     public String getClosingDate() {
         return timeFormatter.formatAsString(closingDate);
+    }
+
+    public void setClosed(boolean closed) {
+        isClosed = closed;
+    }
+
+    public boolean isClosed() {
+        return isClosed;
     }
 
 }
