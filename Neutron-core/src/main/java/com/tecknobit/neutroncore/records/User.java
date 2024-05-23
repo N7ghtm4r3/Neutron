9352bcb9a83f91f3fcbb803d4392f642c6fcb6de
N7ghtm4r3 <maurizio.manuel2003@gmail.com>
2024-05-23 17:42:20 +0200

    Implementing User

--- Neutron-core/src/main/java/com/tecknobit/neutroncore/records/User.java
+++ Neutron-core/src/main/java/com/tecknobit/neutroncore/records/User.java
@@ -2,6 +2,16 @@ package com.tecknobit.neutroncore.records;
 
 public class User extends NeutronItem {
 
+    public enum ApplicationTheme {
+
+        Dark,
+
+        Light,
+
+        Auto
+
+    }
+
     private final String name;
 
     private final String surname;
@@ -14,14 +24,18 @@ public class User extends NeutronItem {
 
     private final String language;
 
+    // TODO: CHECK TO SET AS FINAL
+    private ApplicationTheme theme;
+
     // TODO: TO REMOVE
     public User() {
         this("id", "User", "Name", "user.name@gmail.com", "password",
                 "https://res.cloudinary.com/momentum-media-group-pty-ltd/image/upload/v1686795211/Space%20Connect/space-exploration-sc_fm1ysf.jpg",
-                "Italian");
+                "Italian", ApplicationTheme.Auto);
     }
 
-    public User(String id, String name, String surname, String email, String password, String profilePic, String language) {
+    public User(String id, String name, String surname, String email, String password, String profilePic, String language,
+                ApplicationTheme theme) {
         super(id);
         this.name = name;
         this.surname = surname;
@@ -29,6 +43,7 @@ public class User extends NeutronItem {
         this.password = password;
         this.profilePic = profilePic;
         this.language = language;
+        this.theme = theme;
     }
 
     public String getName() {
@@ -57,6 +72,15 @@ public class User extends NeutronItem {
 
     public String getLanguage() {
         return language;
+    }
+
+    public ApplicationTheme getTheme() {
+        return theme;
+    }
+
+    // TODO: TO REMOVE
+    public void setTheme(ApplicationTheme theme) {
+        this.theme = theme;
     }
 
 }
